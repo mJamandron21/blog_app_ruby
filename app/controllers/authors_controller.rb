e53c7a0b28e3f1adcc59bcf39281cf5e97c45f39
mJamandron21 <mjamandron@gmail.com>
2022-03-15 21:08:22 +0800

    Adding authors (form,new,index,show)

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,51 @@
+class AuthorsController < ApplicationController
+
+    def index   
+        @authors = Author.all
+    end
+
+    def show
+        # @id = params[:id]
+    end
+
+    def new
+        @author = Author.new
+    end
+
+    def edit
+    end
+
+    def create
+        @author = Author.new(author_params)
+
+        if @author.save
+            redirect_to @author
+        else
+            render :new
+        end
+    end
+
+    def update
+        if @author.update(author_params)
+            redirect_to @author
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @author.destroy
+        redirect_to authors_path
+    end
+
+    private
+
+    def set_author
+        @author = Author.find(params[:id])
+    end
+
+    def author_params
+        params.require(:author).permit(:name, :address, :date)
+    end
+
+end
--- app/models/author.rb
+++ app/models/author.rb
@@ -3,14 +3,14 @@ class Author < ApplicationRecord
     validates :name, presence:true, uniqueness: true
 
     #address validation
-    validates :address, presence: true
+    validates :address, presence:true
 
     #date validation
-    validates :date, presence: true
+    validates :date, presence:true
     validate :check_date
 
     def check_date
-        if !self.date.nil? && self.date > Date.today
+        if !self.date.nil? && self.date < Date.today
             errors.add(:date, "This date should not be later than today!")
         end
     end
