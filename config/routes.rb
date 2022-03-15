6c3da3cf8ae9c09b2c79fdb3a05f500bf3bcec4b
mJamandron21 <mjamandron@gmail.com>
2022-03-15 19:21:54 +0800

    Adding Delete function

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ get 'articles/new', to: 'articles#new', as: 'new_article'
 get 'articles/:id', to: 'articles#show', as: 'article'
 get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
 patch 'articles/:id', to: 'articles#update'
+delete 'articles/:id', to: 'articles#destroy'
 
 #resorces :articles (resources sample)
 
