24dd4eed63e08d426825eb5c611137e32da856bf
mJamandron21 <mjamandron@gmail.com>
2022-03-15 19:28:22 +0800

    Adding set article params (before_action)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show,:edit,:update,:destroy]
+
+
     def index
         #render 'articles/show' (to render specific erb)
         @articles = Article.all
@@ -6,7 +10,6 @@ class ArticlesController < ApplicationController
 
     def show
         # @id = params[:id]
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -14,7 +17,6 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
@@ -28,7 +30,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             redirect_to @article
         else
@@ -37,12 +38,15 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
 
     private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
 
     def article_params
         params.require(:article).permit(:title, :content)
