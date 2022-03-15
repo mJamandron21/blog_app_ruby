db6a10d27ba206a303e753e3311490e794c21035
mJamandron21 <mjamandron@gmail.com>
2022-03-15 21:08:38 +0800

    Adding authors into the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
+#articles routes
 get 'articles', to: 'articles#index', as: 'articles'
 post 'articles', to: 'articles#create'
 get 'articles/new', to: 'articles#new', as: 'new_article'
@@ -9,7 +11,16 @@ get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
 patch 'articles/:id', to: 'articles#update'
 delete 'articles/:id', to: 'articles#destroy'
 
+#authors routes
+get 'authors', to: 'authors#index', as: 'authors'
+post 'authors', to: 'authors#create'
+get 'authors/new', to: 'authors#new', as: 'new_author'
+get 'authors/:id', to: 'authors#show', as: 'author'
+get 'authors/:id/edit', to: 'authors#edit', as: 'edit_author'
+patch 'authors/:id', to: 'authors#update'
+delete 'authors/:id', to: 'authors#destroy'
+
 #resorces :articles (resources sample)
 
-root to: 'articles#index' #(to point root page)
+root to: 'authors#index' #(to point root page)
 end
