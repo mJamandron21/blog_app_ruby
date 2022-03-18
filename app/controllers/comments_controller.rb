d88a5abc7b2d1e4b94f848613f932d5edc97d397
mJamandron21 <mjamandron@gmail.com>
2022-03-19 03:37:17 +0800

    Update: Added create/comment_params in comment_con

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,22 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.build
     end
+
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.build(comment_params)
+
+        if @comment.save
+            redirect_to article_comments_path(@article.id)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def comment_params
+        #params.require(:comment).permit(:body,:article_id)
+        params.require(:comment).permit(:body) #can also be without article_id because its @article in def create
+    end
 end
