3bd03afeb3ea0df8df153ff76217ecee9a6debb1
mJamandron21 <mjamandron@gmail.com>
2022-03-28 20:43:36 +0800

    Update: major fix

--- db/migrate/20220328124120_create_user_roles.rb
+++ db/migrate/20220328124120_create_user_roles.rb
@@ -2,8 +2,7 @@ class CreateUserRoles < ActiveRecord::Migration[6.1]
   def change
     create_table :user_roles do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :role
-      t.string :references
+      t.references :role, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_122524) do
+ActiveRecord::Schema.define(version: 2022_03_28_124213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,11 +47,11 @@ ActiveRecord::Schema.define(version: 2022_03_28_122524) do
 
   create_table "user_roles", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "role"
-    t.string "references"
+    t.bigint "role_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "date_assigned"
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
     t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
@@ -62,5 +62,6 @@ ActiveRecord::Schema.define(version: 2022_03_28_122524) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "user_roles", "roles"
   add_foreign_key "user_roles", "users"
 end
