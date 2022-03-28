07f780758ada3254b3412098c22bf7c95b12ac70
mJamandron21 <mjamandron@gmail.com>
2022-03-28 20:20:58 +0800

    Update: db:rollack add UserRole model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_115940) do
+ActiveRecord::Schema.define(version: 2022_03_28_122018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,13 @@ ActiveRecord::Schema.define(version: 2022_03_28_115940) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "roles_users", id: false, force: :cascade do |t|
+  create_table "user_roles", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "role_id", null: false
-    t.index ["role_id", "user_id"], name: "index_roles_users_on_role_id_and_user_id"
-    t.index ["user_id", "role_id"], name: "index_roles_users_on_user_id_and_role_id"
+    t.string "role"
+    t.string "references"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema.define(version: 2022_03_28_115940) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "user_roles", "users"
 end
